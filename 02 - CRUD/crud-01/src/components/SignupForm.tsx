2a066d52ca393e6a02efbe99b73e2faa3c04ed37
BryDev <gransebryan@gmail.com>
2026-01-02 22:48:34 +0800

    removing wrong node  dir

--- 02 - CRUD/crud-01/src/components/SignupForm.tsx
+++ 02 - CRUD/crud-01/src/components/SignupForm.tsx
@@ -33,17 +33,6 @@ const SignupForm: React.FC = () => {
   return (
     <section className="bg-gray-900 text-white min-h-screen">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-white"
-        >
-          <img
-            className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-          />
-          Flowbite
-        </a>
 
         <div className="w-full bg-gray-800 rounded-lg shadow border border-gray-700 md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
