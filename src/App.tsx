2da537cdcd71987ab0a782bafed7d7a1987f0e52
BryDev <gransebryan@gmail.com>
2026-01-06 02:43:17 +0800

    adjusted it

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import InventoryItem from './components/InventoryItem'
 import FunctionalDynamicProps from './components/FunctionalDynamicProps'
 import CottageItem from './components/CottageItem'
+import CottageItemUseStateHook from './components/useState-hook/CottageItemUseStateHook'
 import './App.css'
 
 function App() {
@@ -12,9 +13,11 @@ function App() {
      <InventoryItem name="Shampoo" quantity={10} category="Hygiene" />
      <InventoryItem name="Lotion" quantity={15} category="Hygiene" />
      <InventoryItem name="Toothbrush" quantity={8} category="Hygiene" />
-     <FunctionalDynamicProps name="Cottage #2" price="600 pesos" />*/}
+     <FunctionalDynamicProps name="Cottage #2" price="600 pesos" />
      <CottageItem name="Cottage #2" price={550} description="A big size cottage near the white beach with peso wifi" imgurl="https://picsum.photos/200/300" status='available' />
      <CottageItem name="Cottage #1" price={650} description="A medium size cottage near the white beach with peso wifi" imgurl="https://picsum.photos/200/300" status='unavailable' />
+     */}
+     <CottageItemUseStateHook name="Cottage #2" price={550} description="A big size cottage near the white beach with peso wifi" imgurl="https://picsum.photos/200/300" status='available' />
     </>
   )
 }
