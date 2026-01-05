f3c3668bec8d5307781604395adf646d58c2f2b2
BryDev <gransebryan@gmail.com>
2026-01-05 23:14:57 +0800

    displaying component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import FunctionalDynamicProps from './components/FunctionalDynamicProps'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     
+     <FunctionalDynamicProps name="Cottage #1" price="550 pesos" />
+     <FunctionalDynamicProps name="Cottage #2" price="600 pesos" />
     </>
   )
 }
