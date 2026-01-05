2f8f99b645a2d9317d1bedbcfa9028de76c10aae
BryDev <gransebryan@gmail.com>
2026-01-05 23:04:55 +0800

    welcome message functional component

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Welcome() {
+
+    return (
+        <div className="container">
+            <h1>Welcome to the Functional Component</h1>
+            <p>A simple functional component</p>
+        </div>
+    )
+}
