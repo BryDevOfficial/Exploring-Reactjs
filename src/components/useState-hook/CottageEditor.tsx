87374a1f6e72185e39df1f3f04c0b20e703dadd7
BryDev <gransebryan@gmail.com>
2026-01-08 02:16:50 +0800

    created new component with interface editorProps

---
+++ src/components/useState-hook/CottageEditor.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+interface EditorProps {
+    initialName: string;
+    initialPrice: number;
+}
+
+function CottageEditor() {
+  return (
+    <div>CottageEditor</div>
+  )
+}
+
+export default CottageEditor
