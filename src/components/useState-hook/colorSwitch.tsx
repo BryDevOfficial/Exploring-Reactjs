5246a1b27b138127b243d50019baa13106bd6035
BryDev <gransebryan@gmail.com>
2026-01-07 21:11:22 +0800

    adding ternary operator in colors to change text color of a button when background is change

--- src/components/useState-hook/colorSwitch.tsx
+++ src/components/useState-hook/colorSwitch.tsx
@@ -8,15 +8,33 @@ function ColorSwitch({backgroundColor: defaultColor}: ColorSwitchProps)
 {
     const [currentColor, setCurrentColor] = useState<string>(defaultColor);
 
-    const handleSwitchColor = () => {
+  /*  const handleSwitchColor = () => {
         setCurrentColor((prevColor) =>  prevColor === "red" ? "green" : prevColor === "green" ? "blue" : "red");
-    }
+         // console.log(currentColor);
+    }*/
+
+const handleSwitchColor = () => {
+    setCurrentColor((prev) => {
+        // Define the "Next Step" for every possible color
+        const nextColorMap: Record<string, string> = {
+            red: "green",
+            green: "blue",
+            blue: "red"
+        };
+        
+        return nextColorMap[prev];
+    });
+};
 
 
     return (
 
         <>
-        <button style= {{backgroundColor: currentColor, cursor: "pointer"}} onClick={handleSwitchColor}>Click Me</button>
+        <button style= {
+            {
+            backgroundColor: currentColor, 
+            color: currentColor === "red" ? "white" : currentColor === "blue" ? "yellow" : "purple",
+            cursor: "pointer"}} onClick={handleSwitchColor}>Click Me</button>
         </>
     )
 
